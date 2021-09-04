48da49bc280df6d25d674c60368797c849e83885
Yabluko <83119163+VladVertehel@users.noreply.github.com>
2021-09-04 17:47:22 +0300

    Delete js.js

--- js.js
+++
@@ -1,9 +0,0 @@
-const name = prompt('Enter your name...');
-
-let age = prompt('Enter your age...');
-age = Number(age);
-
-let year = prompt('Enter the current year...');
-year = Number(year);
-
-alert(name + ' - ' + (year - age) + ' y.o.');
