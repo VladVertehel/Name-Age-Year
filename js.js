e498e11328905cb0cc424df8ca4ef758e1678646
Yabluko <83119163+VladVertehel@users.noreply.github.com>
2021-09-04 17:47:29 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+const name = prompt('Enter your name...');
+
+const age = prompt('Enter your year of birth...');
+
+const year = prompt('Enter the current year...');
+
+alert(name + ' - ' + (year - age) + ' y.o.');
